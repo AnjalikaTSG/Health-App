36e79ef0e30044ec70491ed873cb4f1532e0ca7d
AnjalikaTSG <anjalikatsg.21@uom.lk>
2024-11-26 10:43:25 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <Text>Welcome to Health App</Text>
+  );
+}
+
+export default App;
